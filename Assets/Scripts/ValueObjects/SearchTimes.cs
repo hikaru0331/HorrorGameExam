8a382bf7e605c8ff2094f1090c9606dd4b88bb17
hikaru0331 <90139340+hikaru0331@users.noreply.github.com>
2024-03-04 15:51:38 +0900

    [add]びっくりサーチ使用回数の値オブジェクトだけ作成

---
+++ Assets/Scripts/ValueObjects/SearchTimes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+class SearchTimes
+{
+  //値オブジェクトのプロパティはprivateで不変にする。
+  private readonly int time;
+  
+  SearchTimes(int time) {
+    //timeが0以下になる時は、0で固定にする。
+    if(time < 0) time = 0;
+    this.time = time
+  }
+  
+  public int getValue() {
+    return time;
+  }
+
+  public SearchTimes addTime(int addValue) {
+    //値を変更する時は、新しいインスタンスを生成する。
+    return new SearchTimes(time + addValue);
+  }
+}
