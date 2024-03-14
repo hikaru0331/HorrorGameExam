490abafec55852cb6467c116d87d8172e46826bc
hikaru0331 <17731106.koga.cgp@gmail.com>
2024-03-15 01:39:48 +0900

    [fix]リザルト表示の修復

--- Assets/Scripts/ResultScene/GradeDisplayer.cs
+++ Assets/Scripts/ResultScene/GradeDisplayer.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 
 public class GradeDisplayer : MonoBehaviour
 {
-    private int grade;
-    [SerializeField] private TextMeshProUGUI resultText; 
-
     ScoreManager scoreManager;
+    private int grade;
+
+    [SerializeField] private GameObject gradeMetaphorTextObj;
+    [SerializeField] private TextMeshProUGUI gradeMetaphorText;
+
+    [SerializeField] private GameObject gradeTextObj;
+    [SerializeField] private TextMeshProUGUI gradeText; 
+
+    [SerializeField] private GameObject lightStrengthTextObj;
+    [SerializeField] private TextMeshProUGUI lightStrengthText;
+
+    [SerializeField] private GameObject searchTimesTextObj;
+    [SerializeField] private TextMeshProUGUI searchTimesText;
+
+    [SerializeField] private GameObject clearTimeTextObj;
+    [SerializeField] private TextMeshProUGUI clearTimeText;
+
+    private float waitSecond = 0.5f; 
+
+    public static bool canLoadTitleScene = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +33,67 @@ public class GradeDisplayer : MonoBehaviour
         scoreManager = GameObject.FindWithTag("ScoreManager").GetComponent<ScoreManager>();
         GradeCalculater gradeCalculater = new GradeCalculater();
 
-        Debug.Log("ライトの強さ" + scoreManager.GetLightStrength());
-        Debug.Log("サーチ回数" + scoreManager.GetSearchTimes());
-        Debug.Log("クリア時間" + scoreManager.GetClearTime());
         grade = gradeCalculater.GradeCalculate(scoreManager.GetLightStrength(), scoreManager.GetSearchTimes(), scoreManager.GetClearTime());
 
-        resultText.text = "あなたの検定結果は" + grade.ToString() + "級です";
+        gradeText.text = "あなたの検定結果は " + grade.ToString() + " 級です";
+
+        switch(grade)
+        {
+            case 1:
+                gradeMetaphorText.text = "ホラゲー達人レベル";
+                break;
+            case 2:
+                gradeMetaphorText.text = "ホラゲーカジュアル勢レベル";
+                break;
+            case 3:
+                gradeMetaphorText.text = "ホラゲー初心者レベル";
+                break;
+            case 4:
+                gradeMetaphorText.text = "ビビりレベル";
+                break;
+            default:
+                gradeMetaphorText.text = "不明";
+                break;
+        }
+
+        switch(scoreManager.GetLightStrength())
+        {
+            case 40.0f:
+                lightStrengthText.text = "選んだライト：ペンライト";
+                break;
+            case 60.0f:
+                lightStrengthText.text = "選んだライト：懐中電灯";
+                break;
+            case 80.0f:
+                lightStrengthText.text = "選んだライト：業務用懐中電灯";
+                break;
+            default:
+                lightStrengthText.text = "選んだライト：不明";
+                break;
+        }
+
+        searchTimesText.text = "サーチ回数：" + scoreManager.GetSearchTimes().ToString() + "回";
+        clearTimeText.text = "クリア時間：" + scoreManager.GetClearTime().ToString("f2") + "秒";
+
+        StartCoroutine(DisplayTexts());
 
         scoreManager.ResetAllValues();
+    }
+
+    IEnumerator DisplayTexts()
+    {
+        lightStrengthTextObj.SetActive(true);
+        yield return new WaitForSeconds(waitSecond);
+
+        searchTimesTextObj.SetActive(true);
+        yield return new WaitForSeconds(waitSecond);
+
+        clearTimeTextObj.SetActive(true);
+        yield return new WaitForSeconds(waitSecond);
+
+        gradeTextObj.SetActive(true);
+        gradeMetaphorTextObj.SetActive(true);
+        
+        canLoadTitleScene = true;
     }
 }
